af0ba5a60864904cbddae54c4377b8a22d1897dc
Senapp Studios <senappstudios@gmail.com>
2025-03-24 21:15:45 +0900

    New algorithms and UI updates

--- src/components/RenderView.tsx
+++ src/components/RenderView.tsx
@@ -7,7 +7,7 @@ type Props = {
 }
 
 export const RenderView: React.FC<Props> = ({  }) => {
-    const [visualArray, setVisualArray] = useState([3, 1, 8, 0, 4, 7, 2, 6, 5, 9]);
+    const [visualArray, setVisualArray] = useState([3, 1, 8, 5, 9, 6]);
     const [currentIndex, setCurrentIndex] = useState(-1);
     const [targetIndex, setTargetIndex] = useState(-1);
     
@@ -19,12 +19,60 @@ export const RenderView: React.FC<Props> = ({  }) => {
     const [setFromStoredAnimation, setSetFromStoredAnimation] = useState(false);
     const [highlightStoredValue, setHighlightStoredValue] = useState(false);
 
+    const [isSorting, setIsSorting] = useState(false);
+    const [sliderValue, setSliderValue] = useState(50)
 
-    const animationSpeed = 0.2;
-    const highlightingSpeed = 0.1;
+
+    const animationSpeed = 0.05 / (sliderValue / 100);
+    const highlightingSpeed = animationSpeed / 2;
     const spacing = 50;
 
+    // Logarithmic
+
+    const partition = async (array: number[], left: number, right: number): Promise<[number[], number]> => {
+        let newArray = [...array];
+        const pivotValue = await storeElement(newArray, right);
+        let pivotIndex = left;
+        for (let i = left; i < right; i++) {
+            if (await checkValue(newArray, i) < await checkStoredValue(pivotValue)) {
+                if (i !== pivotIndex) {
+                    newArray = await switchElements(newArray, i, pivotIndex);
+                    setVisualArray(newArray);
+                }
+                resetPointers();
+                pivotIndex++;
+            }
+        }
+        if (right !== pivotIndex) {
+            newArray = await switchElements(newArray, pivotIndex, right);
+            setVisualArray(newArray);
+        }
+        resetPointers();
+        return [newArray, pivotIndex];
+    };
+
+    const quickSortFunction = async (array: number[], left = 0, right = array.length - 1) => {
+        let newArray = [...array];
+        let newPivotIndex = 0;
+        if (left < right) {
+            [newArray, newPivotIndex] = await partition(newArray, left, right);
+            newArray = await quickSortFunction(newArray, left, newPivotIndex - 1);
+            newArray = await quickSortFunction(newArray, newPivotIndex + 1, right);
+        }
+        return newArray;
+    };
+
+    const quickSortVisual = async () => {
+        setIsSorting(true);
+        quickSortFunction([...visualArray]).then(() => {
+            sortingFinished();
+        });
+    }
+
+    //Quadratic
+
     const bubbleSortVisual = async () => {
+        setIsSorting(true);
         let swapped = true;
         let newArray = [...visualArray];
         while (swapped) {
@@ -45,6 +93,7 @@ export const RenderView: React.FC<Props> = ({  }) => {
     };
 
     const selectionSortVisual = async () => {
+        setIsSorting(true);
         let newArray = [...visualArray];
         for (let i = 0; i < newArray.length; i++) {
             let minIndex = i;
@@ -66,6 +115,7 @@ export const RenderView: React.FC<Props> = ({  }) => {
     };
 
     const insertionSortVisual = async () => {
+        setIsSorting(true);
         let newArray = [...visualArray];
         for (let i = 1; i < newArray.length; i++) {
             let key = await storeElement(newArray, i);
@@ -85,6 +135,44 @@ export const RenderView: React.FC<Props> = ({  }) => {
         sortingFinished();
     };
 
+    // Bad
+    const bogoSortVisual = async () => {
+        setIsSorting(true);
+        let newArray = [...visualArray];
+        for (let i = 0; i < newArray.length; i++) {
+            if (i < newArray.length - 1 && (await checkValue(newArray, i) <= await checkValue(newArray, i + 1))) {
+                resetPointers();
+            } else if (i == newArray.length - 1) {
+                continue;
+            } else {
+                i = -1;
+                shuffle(newArray);
+                setVisualArray(newArray);
+            }
+        }
+
+        setVisualArray(newArray);
+        sortingFinished();
+    }
+
+    const shuffle = (array: number[]): void => {
+        let currentIndex = array.length;
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+          // Pick a remaining element...
+          let randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      }
+
+    const randomizeNumbers = () => {
+        const newArray = Array.from({ length: Math.max(4, Math.floor(Math.random() * 16)) }, () => Math.floor(Math.random() * 100));
+        setVisualArray(newArray);
+    };
+
     const sortingFinished = () => {
         setStoredValueVisual(undefined);
         setSetAnimation(false);
@@ -93,7 +181,10 @@ export const RenderView: React.FC<Props> = ({  }) => {
         setSetFromStoredAnimation(false);
         setHighlightStoredValue(false);
         resetPointers();
-        alert('Sorting finished!');
+        setIsSorting(false);
+        setTimeout(() => {
+            alert('Sorting finished!');
+        }, 500);
     }
 
     const resetPointers = () => {
@@ -118,8 +209,8 @@ export const RenderView: React.FC<Props> = ({  }) => {
         return storedValue;
     }
 
-    const checkValue = async (array: number[], index: number): Promise<number> => {
-        setCurrentIndex(index);
+    const checkValue = async (array: number[], index: number, visualNumber: number = index): Promise<number> => {
+        setCurrentIndex(visualNumber);
         await new Promise(resolve => setTimeout(resolve, highlightingSpeed * 1000));
         return array[index];
     }
@@ -154,22 +245,63 @@ export const RenderView: React.FC<Props> = ({  }) => {
         return newArray;
     }
 
-    return (<>
-        <Button onClick={() => bubbleSortVisual()} label='Bubble Sort'></Button>
-        <Button onClick={() => selectionSortVisual()} label='Selection Sort'></Button>
-        <Button onClick={() => insertionSortVisual()} label='Insertion Sort'></Button>
+    const maxValue = Math.max(...visualArray)
+    const color = "#d8adff";
 
+    return (<>
+        <div className={css.buttonContainer}>
+            <div className={css.sortTitle}>
+                Logarithmic Sorting:
+                <div className={css.sortContainer}>
+                    <Button disabled={isSorting} onClick={() => quickSortVisual()} label='Quick Sort'></Button>
+                </div>
+            </div>
+            <div className={css.sortTitle}>
+                Quadratic Sorting:
+                <div className={css.sortContainer}>
+                    <Button disabled={isSorting} onClick={() => bubbleSortVisual()} label='Bubble Sort'></Button>
+                    <Button disabled={isSorting} onClick={() => selectionSortVisual()} label='Selection Sort'></Button>
+                    <Button disabled={isSorting} onClick={() => insertionSortVisual()} label='Insertion Sort'></Button>
+                </div>
+            </div>
+            <div className={css.sortTitle}>
+                "Terrible" Sorting:
+                <div className={css.sortContainer}>
+                    <Button disabled={isSorting} onClick={() => bogoSortVisual()} label='Bogo Sort'></Button>
+                </div>
+            </div>
+            <div className={css.sortTitle}>
+                Randomize Numbers:
+                <div className={css.sortContainer}>
+                    <Button disabled={isSorting} onClick={() => randomizeNumbers()} label='Randomize Numbers'></Button>
+                </div>
+            </div>
+            <div className={css.sortTitle}>
+                Animation Speed:
+                <div className={css.sortContainer}>
+                    <input disabled={isSorting} type="range" min="1" max="100" value={sliderValue} onChange={(e) => setSliderValue(parseInt(e.target.value))} className={css.slider} id="myRange"></input>
+                </div>
+            </div>
+        </div>
+        <div className={css.barContainer}>
+                 {visualArray.map((element, index) => {
+                   let height = 75;
+                   return <div style={{ "height": `${Math.max(1, height * (element/maxValue))}px`}} className={`${css.bar}`.trimEnd()}></div>;
+               })
+           }
+        </div>
         <div className={css.container}>
             <>
             {
                 storedValueVisual !== undefined && <div style={{
-                    "backgroundColor": highlightStoredValue ? "green" : "white"
+                    "backgroundColor": highlightStoredValue ? color : "white"
                 }} className={`${css.cell} ${css.storedCell}`.trimEnd()}>{storedValueVisual}</div>
             }
           {
               visualArray.map((element, index) => {
-                  let isCurrent = index === currentIndex;
+                let isCurrent = index === currentIndex;
                 let isTarget = index === targetIndex;
+
                 if (isCurrent && switchAnimation) {
                     return <div style={{
                         "--target": `${(targetIndex - currentIndex) * spacing}px`,
@@ -181,7 +313,7 @@ export const RenderView: React.FC<Props> = ({  }) => {
                         "--animationSpeed": `${animationSpeed}s`
                     } as React.CSSProperties} className={css.cell + " " + css.targetCell + " " + css.animationSecondary}>{element}</div>;
                 }
-                
+
                 if (isCurrent && setAnimation) {
                     const offset = visualArray.length / 2 * -spacing + 0.5 * spacing;
                     return <><div style={{
@@ -189,7 +321,7 @@ export const RenderView: React.FC<Props> = ({  }) => {
                         "--start": `${offset + spacing * currentIndex}px`,
                         "--animationSpeed": `${animationSpeed}s`,
                         "position": "absolute"
-                    } as React.CSSProperties} className={css.cell + " " + css.currentCell + " " + css.animationPrimary}>{element}</div>
+                    } as React.CSSProperties} className={css.cell + " " + css.currentCell + " " + css.animationSet}>{element}</div>
                     <div className={`${css.cell}`.trimEnd()}>{element}</div>
                     </>;
                 }
@@ -205,7 +337,7 @@ export const RenderView: React.FC<Props> = ({  }) => {
                     return <><div style={{
                         "--start": `${offset + spacing * currentIndex}px`,
                         "--animationSpeed": `${animationSpeed}s`,
-                        "backgroundColor": "green",
+                        "backgroundColor": color,
                         "position": "absolute"
                     } as React.CSSProperties} className={css.cell + " " + css.animationStore}>{element}</div>
                     <div className={`${css.cell}`.trimEnd()}>{element}</div>
@@ -217,7 +349,7 @@ export const RenderView: React.FC<Props> = ({  }) => {
                     return <><div style={{
                         "--target": `${offset + spacing * targetIndex}px`,
                         "--animationSpeed": `${animationSpeed}s`,
-                        "backgroundColor": "green",
+                        "backgroundColor": color,
                         "position": "absolute"
                     } as React.CSSProperties} className={css.cell + " " + " " + css.animationSetFromStored}>{storedValueVisual}</div>
                     <div className={`${css.cell} ${isTarget && css.targetCell}`.trimEnd()}>{element}</div>
--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -5,11 +5,14 @@ type Props = {
     label?: string;
     small?: boolean;
     className?: string;
+    disabled?: boolean;
     onClick: () => void;
 }
 
-export const Button: React.FC<Props> = ({ label = '', className = '', small = false, onClick }) => (
-    <div className={`${css.button} ${small && css.smallButton} ${className}`.trimEnd()} onClick={onClick}>
+export const Button: React.FC<Props> = ({ label = '', className = '', disabled = false, small = false, onClick }) => (
+    <div className={`${css.button} ${small && css.smallButton} ${disabled && css.disabled} ${className}`.trimEnd()} onClick={() => {
+        if (!disabled) onClick();
+    }}>
         {label}
     </div>
 );
